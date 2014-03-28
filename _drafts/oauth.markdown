d540956f3bf043329eb9125f5615902b065ab4d3
David Asabina <david@supr.nu>
2014-03-28 01:49:11 +0100

    Refactor auth game

--- _drafts/oauth.markdown
+++ _drafts/oauth.markdown
@@ -1,7 +1,7 @@
 ---
 layout: post
-title: Another OAuth Article
-description: OAuth explained in what I understand to be Layman&rsquo;s terms. Mind you, I have never met the man.
+title: Another Auth Article
+description: My take on different auth strategies available in what I consider to be Layman&rsquo;s terms. Mind you, I have never met the man.
 type: web
 tags:
   - web
@@ -18,23 +18,52 @@ Whilst acting as tech-lead on several web-related projects I find myself
 explaining OAuth ever too often to fellow team members who can read perfectly 
 well. 
 
-As far as authentication options go OAuth might be somewhat complex, but it
-is not difficult to understand. The complexity is in the different steps.
+As far as authentication options different flavors are available for those
+wo want to be picky, and as always there is no single right answer for many
+problems. Based on you requirements, required time-to-market you may want to 
+choose one option over the other.
 
-## Dancing for Hackers
+## The Thing About Auth
+
+For the sake of keeping things complete I need to point out the difference 
+between _authentication_ and _authorization_.
+
+Authentication has to deal with the _is this agent realy who it says it is?_
+question, while authorization seeks to answer _is this agent allowed to do
+what it is trying to do?_.
+
+A real-life analogy could be found on airports. Airports have authentication 
+procedures in place to confirm that the passport is not forged and that the 
+person at the counter is really the feller or gal on the passport. 
+After authentication you are allowed to enter the terminal en route
+to your flight. Authorization takes place at multiple points. First of all
+the passenger is not walking around with a employee badge which effectively
+denies the passenger access to certain areas in the airport. Furthermore, 
+the passenger has to be authorized to board an airplane after presenting the 
+ground steward or stewardess the proper documents (boarding pass and passport).
+
+As you see _who am I_ and _what may I do_ are the two different questions which 
+authentication and authorization should be able to answer.
+
+## Keeping it Simple &dash; Basic
+As far as authentication goes, Basic is the simplest way to a quick victory.
+We supply the system a handle and a secret (username and password) and it 
+grants or denies us access with the given credentials.
+
+The steps in Basic auth as as follows:
+
+ - the client requests the handle and secret (credentials)
+ - the client prepares a request including the auth details
+ - the client sends the request to the server
+ - the server receives the request
+ - the server searches for a match for the given credentials
+ - the server executes the request if the credentials are matched and places the results into a prepared response
+ - the server sends the response to the client
+
 <div class="element game" id="basic-auth-game">
-  <div class="client">
-    <pre>/ o\  simple demo of Basic auth</pre>
-    <pre>\_ /  in a somewhat cheesy way :D</pre>
-    <pre>&nbsp;&lt;|</pre>
-    <pre>&nbsp;&lt;|</pre>
-    <pre>&nbsp;&lt;|</pre>
-    <pre>&nbsp;`</pre>
-  </div>
-  <div class="cable">
-  </div>
-  <div class="server">
-  </div>
+  <div class="server"></div>
+  <div class="cable"></div>
+  <div class="client"></div>
   <div class="login_dialog">
     <input class="handle" placeholder="handle"></input>
     <input class="secret" type="password" placeholder="secret"></input>
@@ -43,6 +72,20 @@ is not difficult to understand. The complexity is in the different steps.
   <script type="text/javascript" src="/play/auth/deps/require.js" data-main="/play/auth/basic.js"></script>
 </div>
 
+## Dancing for Hackers (OAuth)
+
+OAuth might be somewhat complex, but it is not difficult to understand.
+The complexity is in the different steps.
+
+###
+
+ - get authorization grant from resource owner
+ - obtain access token with authorization grant
+   - scope
+   - duration
+
 [rfc-basic-base64]: https://tools.ietf.org/html/rfc2617#page-19
 [oauth-rfc]: http://tools.ietf.org/html/rfc5849
 [ascii-art]: http://www.geocities.com/spunk1111/small.htm
+[oauth-bearer]: http://tools.ietf.org/html/rfc6750
+[stripe-auth]: https://stripe.com/docs/api/curl#authentication
--- play/auth/basic.js
+++ play/auth/basic.js
@@ -4,7 +4,6 @@ require([
   '/play/auth/deps/backbone-min.js',
   '/play/auth/deps/jquery-1.10.2.min.js'
 ], function(underscore, backbone, jquery) {
-  console.log('let us go');
   Backbone.$ = $;
 
   var game = {};
@@ -13,7 +12,6 @@ require([
   var LogView = Backbone.View.extend({
     tagName: "pre",
     initialize: function() {
-      console.log("new log entry");
     },
     template: _.template("<%= message %>"),
     render: function() {
@@ -45,53 +43,109 @@ require([
       var secret = $('#basic-auth-game .login_dialog .secret').val();
       game.trigger("login", [handle, secret]);
     },
-    updateConsole: function(view) {
-      this.$(".client").append(view.render().el);
-      this.$(".client")[0].scrollTop = this.$(".client")[0].scrollHeight;
+    updateConsole: function(target, view) {
+      this.$("." + target).append(view.render().el);
+      this.$("." + target)[0].scrollTop = this.$("." + target)[0].scrollHeight;
     },
-    hideDialog: function(next) {
-      $('.login_dialog').addClass('animated slideOutRight');
-      $('.login_dialog').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        console.log('done');
+    animateToSegue: function(object, animation, next) {
+      $('.' + object).addClass('animated ' + animation);
+      $('.' + object).on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
         next();
       });
-    }
-    showDialog: function(next) {
-      $('.login_dialog').addClass('animated slideInRight');
-      $('.login_dialog').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        console.log('done');
-        next();
-      });
+    },
+    show: function(object, next) {
+      _.defer(function(view) {
+        switch(object) {
+        case 'login_dialog':
+          view.animateToSegue(object, 'bounceIn', next);
+          break;
+        default:
+          view.animateToSegue(object, 'fadeInDown', next);
+        }
+      }, this);
+    },
+    hide: function(object, next) {
+      _.defer(function(view) {
+        switch(object) {
+        case 'login_dialog':
+          view.animateToSegue(object, 'bounceOut', next);
+          break;
+        default:
+          view.animateToSegue(object, 'fadeOutDown', next);
+        }
+      }, this);
     }
   });
 
-  var gamescreen = new GameView({el: 'div #basic-auth-game'});
+  var screen = new GameView({el: 'div #basic-auth-game'});
 
   var history = new Backbone.Collection;
   history.on('add', function(activity) {
-    console.log('logging new item');
-    gamescreen.updateConsole(new LogView({ model: activity }));
+    screen.updateConsole('client', new LogView({ model: activity }));
+  });
+
+  game.on("intro", function() {
+    console.log('intro');
+    history.add({ message: "/ o\\  simple demo of Basic auth" });
+    history.add({ message: "\\_ /  " });
+    history.add({ message: "&nbsp;&lt;|" });
+    history.add({ message: "&nbsp;&lt;|" });
+    history.add({ message: "&nbsp;&lt;|" });
+    history.add({ message: "&nbsp;`" });
+    screen.show('login_dialog', function() {
+    });
   });
 
   game.on("login", function(msg) {
     console.log("login triggered for " + JSON.stringify(msg));
-    history.add({ message: "Beginning auth circus..." });
-    gamescreen.hideDialog(function() {
-      game.trigger("compose_base64", msg);
+    // TODO: disable login button from this point onward
+    screen.hide('login_dialog', function() {
+      game.trigger("calc_base64", msg);
     });
   });
   
-  game.on("compose_base64", function(msg) {
+  game.on("calc_base64", function(msg) {
     var str = (msg[0] + ":" + msg[1]);
     var base = window.btoa(unescape(encodeURIComponent(str)));
     history.add({ 
       message: ( "Base64(" + str + ") = " + base)
     });
+    game.trigger("compose_request", { auth: "Basic " + base } );
   });
 
   game.on("compose_request", function(data) {
-    console.log("WIP");
-  }
+    if(typeof(data) != "object") return;
 
+    if(!data.method) data.method = "GET"
+    if(!data.resource) data.resource= "/resource"
+    if(!data.host) data.host = "example.com"
+    
+    var requestString = "";
+    if(data.host) requestString += data.method + " " + data.resource + " HTTP/1.1\n"
+    if(data.host) requestString += "  Host: " + data.host + "\n"
+    if(data.auth) requestString += "  Authorization: " + data.auth + "\n"
+    history.add({
+      message: ( requestString )
+    });
+    var headers = {
+      "Authorization": data.auth
+    }
+
+    _.delay(function() {
+      game.trigger("send_request", { 
+        headers: headers,
+        body: null,
+        params: null
+      });
+    }, 2000);
+  });
+
+  game.on("send_request", function(req) {
+    screen.hide('client', function() {
+      console.log('sent');
+    });
+  });
+
+  game.trigger("intro");
   //game.trigger("login", ["user", "pass"]);
 });
