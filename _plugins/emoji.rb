d24dd4e6f4cf6f8c8bd9b4400be2a2b1895b056a
David Asabina <david@supr.nu>
2015-09-17 19:25:19 +0200

    bypassing the `gemoji` plugin

--- _plugins/emoji.rb
+++ _plugins/emoji.rb
@@ -37,11 +37,11 @@ module Jekyll
       end
 
       content.to_str.gsub(/:([a-z0-9\+\-_]+):/) do |match|
-        if Emoji.names.include?($1) and emoji_dir
-          '<img alt="' + $1 + '" src="' + emoji_dir + "/#{$1}.png" + '" class="emoji" />'
-        else
+#        if Emoji.names.include?($1) and emoji_dir
+#          '<img alt="' + $1 + '" src="' + emoji_dir + "/#{$1}.png" + '" class="emoji" />'
+#        else
           match
-        end
+#        end
       end
     end
 
